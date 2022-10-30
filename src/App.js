24a3f634d191b0d5cba69444e5139c623f155a78
Elizabeth Sandri <elizabethsandri18@gmail.com>
2022-10-30 18:03:48 +0000

    Added main Weather detais components
    As well as added the style of these components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Weather from './Weather';
 import SearchEngine from './SearchEngine';
+import CurrentLocationWeather from './CurrentLocationWeather';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       </header>
       <SearchEngine />
       <Weather city="Tokyo" />
+      <CurrentLocationWeather />
     </div>
   );
 }
